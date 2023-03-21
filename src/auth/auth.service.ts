741faf709e56af3ecc10d907fcb8348d2772f8b4
Johan Torfs <johan@pajamallama.be>
2023-03-21 15:29:39 +0100

    return user on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,9 +21,9 @@ export class AuthService {
     }
 
     async login(user: User) {
-        const payload = { email: user.email, id: user.id };
+        const payload = { email: user.email, sub: user.id };
         return {
-        user: payload,
+        user: { email: user.email, id: user.id, role: user.role.name },
         access_token: this.jwtService.sign(payload),
         };
     }
