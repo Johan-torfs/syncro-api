7901b7ee0854ea004a1cac82f1c03842e51734bd
Johan Torfs <johan@pajamallama.be>
2023-03-21 15:19:21 +0100

    import UserModule

--- src/assets/assets.module.ts
+++ src/assets/assets.module.ts
@@ -3,9 +3,10 @@ import { AssetsService } from './assets.service';
 import { AssetsController } from './assets.controller';
 import { Asset } from './entities/asset.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Asset])],
+  imports: [TypeOrmModule.forFeature([Asset]), UsersModule],
   controllers: [AssetsController],
   providers: [AssetsService]
 })
--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,10 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment]), UsersModule],
   controllers: [CommentsController],
   providers: [CommentsService]
 })
